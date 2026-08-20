5bf2ba2942a96547dc80104bcd0c40eadc46dc15
Lim En Xi <limenxi07@gmail.com>
2026-08-20 09:42:54 +0800

    Level-1: Echo commands until bye

--- src/main/java/Serangooner.java
+++ src/main/java/Serangooner.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Serangooner {
     public static void main(String[] args) {
         String banner = "  ____   U _____ u   ____        _      _   _     ____    U  ___ u   U  ___ u  _   _   U _____ u   ____     \n"
@@ -7,11 +9,31 @@ public class Serangooner {
                 + " |____/>> |_____|   |_| \\_\\  /_/   \\_\\ |_| \\_|   \\____| \\_)-\\___/  \\_)-\\___/  |_| \\_|   |_____|   |_| \\_\\   \n"
                 + "  )(  (__)<<   >>   //   \\\\_  \\\\    >> ||   \\\\,-._)(|_       \\\\         \\\\    ||   \\\\,-.<<   >>   //   \\\\_  \n"
                 + " (__)    (__) (__) (__)  (__)(__)  (__)(_\")  (_/(__)__)     (__)       (__)   (_\")  (_/(__) (__) (__)  (__)  \n";
+        String divider = "━━━ . °‧ \uD80C\uDD9D \uD80C\uDD9F \uD80C\uDD9E ·｡";
+
+        /* intro */
+        System.out.println(divider);
         System.out.println(banner);
-        System.out.println("____________________________________________________________");
         System.out.println("serangooner at your service. what's up?");
-        System.out.println("____________________________________________________________");
+        System.out.println("type 'bye' to end chat :(");
+        System.out.println(divider);
+
+        /* listen to user input */
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                break; // exit
+            }
+            // default command: echo
+            System.out.println(divider);
+            System.out.println("echo: " + command);
+            System.out.println(divider);
+        }
+
+        /* exit */
+        System.out.println(divider);
         System.out.println("logging off...");
-        System.out.println("____________________________________________________________");
+        System.out.println(divider);
     }
 }
